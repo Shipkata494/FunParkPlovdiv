4a4563473da0e1e9c1c790e2fde6f8b1d7f42619
Shipkata494 <toni_6ipkata@abv.bg>
2023-12-22 11:58:01 +0200

    Add project files.

---
+++ FunParkPlovdiv/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FunParkPlovdiv.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ FunParkPlovdov.Common/GeneralApplicationConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace FunParkPlovdiv.Common
+{
+    public class GeneralApplicationConstants
+    {
+       public static class YearOfRelesed
+        {
+            public const int RelesedYear = 2023;
+        }
+    }
+}
