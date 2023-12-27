0f09435af9dc0d380dd33154f3edc276fc27c6ea
Shipkata494 <toni_6ipkata@abv.bg>
2023-12-27 10:17:41 +0200

    Add EntityFramework. Add DbContext.

---
+++ FunParkPlovdiv.Data/FunParkPlovdivDbContext.cs
@@ -0,0 +1,15 @@
+﻿namespace FunParkPlovdiv.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    public class FunParkPlovdivDbContext : DbContext
+    {
+        public FunParkPlovdivDbContext(DbContextOptions<FunParkPlovdivDbContext> options) :base(options)
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+           
+        }
+    }
+}
